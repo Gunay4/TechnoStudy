22cf12fa78ddfe50427902dd0682a8c6c5091cfb
Gunay4 <gun1githb@gmail.com>
2024-10-13 14:43:48 +0200

    US01 completed 2

--- src/US01/US01_TC01.java
+++ src/US01/US01_TC01.java
@@ -1,17 +1,15 @@
 package US01;
 
-import Utilities.BaseDriver;
 import Utilities.BaseDriverParameter;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class US01_TC01 extends BaseDriverParameter {
 
     @Test()
-    public void DropdownMenu(){
-        Pom1 pm1=new Pom1(driver);
+    public void DropdownMenu() {
+        Pom1 pm1 = new Pom1(driver);
         driver.get("https://techno.study/tr");
 
         wait.until(ExpectedConditions.elementToBeClickable(pm1.courses)).click();
@@ -22,22 +20,25 @@ public class US01_TC01 extends BaseDriverParameter {
 
         wait.until(ExpectedConditions.visibilityOf(pm1.sdet)).click();
         Assert.assertTrue(driver.getCurrentUrl().contains("sdet"));
-        driver.navigate().to("https://techno.study/tr");
+        pm1.technoStudybutton.click();
 
+        wait.until(ExpectedConditions.textToBePresentInElement(pm1.mainPageMessage, "Yeni alanlar keşfet."));
         actions.moveToElement(pm1.courses).build().perform();
-        wait.until(ExpectedConditions.visibilityOf(pm1.androidDev)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(pm1.androidDev)).click();
         Assert.assertTrue(driver.getCurrentUrl().contains("androidapp"));
-        driver.navigate().to("https://techno.study/tr");
+        pm1.technoStudybutton.click();
 
+        wait.until(ExpectedConditions.textToBePresentInElement(pm1.mainPageMessage, "Yeni alanlar keşfet."));
         actions.moveToElement(pm1.courses).build().perform();
         wait.until(ExpectedConditions.visibilityOf(pm1.dataScience)).click();
         Assert.assertTrue(driver.getCurrentUrl().contains("veribilimi"));
-        driver.navigate().to("https://techno.study/tr");
+        pm1.technoStudybutton.click();
 
+        wait.until(ExpectedConditions.textToBePresentInElement(pm1.mainPageMessage, "Yeni alanlar keşfet."));
         actions.moveToElement(pm1.courses).build().perform();
         wait.until(ExpectedConditions.visibilityOf(pm1.jobCenter)).click();
         Assert.assertTrue(driver.getCurrentUrl().contains("jobcenter"));
-        driver.navigate().to("https://techno.study/tr");
+        pm1.technoStudybutton.click();
 
     }
 }
