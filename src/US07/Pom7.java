8df32adf51fac6183f3923f06bc5f9f4ac408afe
Ali <alikilinc.wk@gmail.com>
2024-10-17 16:50:21 +0300

    US07_TC01 tamamlandı.

--- src/US07/Pom7.java
+++ src/US07/Pom7.java
@@ -1,4 +1,26 @@
 package US07;
 
+import Utilities.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 public class Pom7 {
+    public Pom7() {
+        PageFactory.initElements(BaseDriver.driver,this);
+    }
+
+    @FindBy(xpath= "//*[@data-elem-type='form']")
+    public WebElement scrollToForm;
+
+    @FindBy(xpath = "//a[text()='Kullanım Şartlarını']")
+    public WebElement termOfUse;
+
+    @FindBy(xpath = "//strong")
+    public List<WebElement> articles;
+
+    @FindBy(css = "[field='title']>div")
+    public WebElement title;
 }
--- src/US07/US07_TC01.java
+++ src/US07/US07_TC01.java
@@ -1,4 +1,31 @@
 package US07;
 
-public class US07_TC01 {
+import Utilities.BaseDriver;
+import Utilities.Tools;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.Set;
+
+public class US07_TC01 extends BaseDriver {
+
+    @Test
+    public void TC01() {
+        Pom7 pom = new Pom7();
+        driver.get("https://techno.study/tr/");
+        String homeID = driver.getWindowHandle();
+        js.executeScript("arguments[0].scrollIntoView();", pom.scrollToForm);
+        pom.termOfUse.click();
+        Set<String> tabIds = driver.getWindowHandles();
+        for (String e : tabIds)
+            if (!e.equals(homeID))
+                driver.switchTo().window(e);
+        wait.until(ExpectedConditions.urlToBe("https://techno.study/tr/terms"));
+        Assert.assertFalse(pom.articles.isEmpty());
+        for (WebElement articles : pom.articles) {
+            Assert.assertFalse(articles.getText().isEmpty());
+        }
+        Assert.assertTrue(pom.title.getText().equalsIgnoreCase("kullanım şartları"));
+    }
 }
